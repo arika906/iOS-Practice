6585c90675a5cc4ef7329e05d935e059d4b666ee
arika906 <arika.boshra@gmail.com>
2019-12-12 16:32:28 +0600

    Add files via upload

---
+++ aScrollView/aScrollView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aScrollView
+//
+//  Created by MAC MINI on 12/12/19.
+//  Copyright © 2019 MAC MINI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
