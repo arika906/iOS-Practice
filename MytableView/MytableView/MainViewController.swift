9c6fc1ee18939cd029d192638ae0ca4aa1115d3f
arika906 <arika.boshra@gmail.com>
2019-12-24 17:13:35 +0600

    Add files via upload

---
+++ MytableView/MytableView/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MytableView
+//
+//  Created by Admin on 12/8/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
