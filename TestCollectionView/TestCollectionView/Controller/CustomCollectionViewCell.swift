606ece8ced3dbc19fbb3abb7798dc413fd29ee24
arika906 <arika.boshra@gmail.com>
2019-12-24 17:12:50 +0600

    Add files via upload

---
+++ TestCollectionView/TestCollectionView/Controller/CustomCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionViewCell.swift
+//  TestCollectionView
+//
+//  Created by Admin on 12/5/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+}
